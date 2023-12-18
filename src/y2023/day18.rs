6806a48a9a17de1824ee961e3cb305fed1e2d398
Jesper Eskilson <jesper.eskilson@klarna.com>
2023-12-18 07:20:19 +0100

    day 18 part 2

--- src/y2023/day18.rs
+++ src/y2023/day18.rs
@@ -1,127 +1,70 @@
-use hashbrown::HashMap;
 use itertools::Itertools;
+use nd_vec::vector;
 
 use crate::Solver;
 pub struct Solution;
 impl Solver for Solution {
     fn solve(&self, input: &String) -> (String, String) {
-        (solve_p1(input).to_string(), String::new())
+        (solve_p1(input).to_string(), solve_p2(input).to_string())
     }
 }
 
-type RowCol = (i32, i32);
-type RGB = (i32, i32, i32);
-
-fn solve_p1(input: &str) -> usize {
-    let mut map: HashMap<RowCol, RGB> = HashMap::new();
-    map.insert((0, 0), (0, 0, 0));
-
-    input
+fn solve_p1(input: &str) -> i64 {
+    let instructions: Vec<(char, _)> = input
         .lines()
         .map(|line| {
-            let (dir, len, rgb) = line
+            let (dir, len) = line
                 .split(|c| c == ' ' || c == '(' || c == '#' || c == ')')
                 .filter(|s| s.len() > 0)
                 .next_tuple()
                 .unwrap();
-            let dir = dir.chars().next().unwrap();
-            let len = len.parse::<i32>().unwrap();
-            let r = &rgb[0..2];
-            let g = &rgb[2..4];
-            let b = &rgb[4..6];
+            (dir.chars().next().unwrap(), len.parse().unwrap())
+        })
+        .collect();
+
+    shoelace(instructions)
+}
+
+fn solve_p2(input: &str) -> i64 {
+    let instructions: Vec<(char, _)> = input
+        .lines()
+        .map(|line| {
+            let (_, hexstr) = line.split_once('#').unwrap();
             (
-                dir,
-                len,
-                (
-                    i32::from_str_radix(r, 16).unwrap(),
-                    i32::from_str_radix(g, 16).unwrap(),
-                    i32::from_str_radix(b, 16).unwrap(),
-                ),
+                match hexstr[5..6].chars().next().unwrap() {
+                    '0' => 'R',
+                    '1' => 'D',
+                    '2' => 'L',
+                    '3' => 'U',
+                    _ => unreachable!(),
+                },
+                i64::from_str_radix(&hexstr[0..5], 16).unwrap(),
             )
         })
-        .fold((0, 0), |pos, (dir, len, rgb)| {
-            let (row_delta, col_delta) = match dir {
-                'U' => (-1, 0),
-                'L' => (0, -1),
-                'D' => (1, 0),
-                'R' => (0, 1),
-                _ => unreachable!(),
-            };
+        .collect();
 
-            (0..len).fold(pos, |p, _| {
-                map.insert(p, rgb);
-                let (row, col) = p;
-                (row + row_delta, col + col_delta)
-            })
-        });
-
-    // let max_col: i32 = *map.keys().map(|(_row, col)| col).max().unwrap();
-    // let max_row: i32 = *map.keys().map(|(row, _col)| row).max().unwrap();
-    // let min_col: i32 = *map.keys().map(|(_row, col)| col).min().unwrap();
-    // let min_row: i32 = *map.keys().map(|(row, _col)| row).min().unwrap();
-
-    // println!();
-    // for row in min_row..=max_row {
-    //     for col in min_col..=max_col {
-    //         if let Some(_rgb) = map.get(&(row, col)) {
-    //             print!("#");
-    //         } else {
-    //             print!(".");
-    //         }
-    //     }
-    //     println!();
-    // }
-
-    flood_fill((1, 1), &mut map);
-    map.len()
+    shoelace(instructions)
 }
 
-fn flood_fill(pos: RowCol, map: &mut HashMap<RowCol, RGB>) {
-    let mut unfilled: Vec<RowCol> = vec![pos];
+fn shoelace(instructions: Vec<(char, i64)>) -> i64 {
+    let mut pos = vector!(0, 0);
+    let mut perimeter = 0;
+    let mut area = 0;
 
-    while let Some(p) = unfilled.pop() {
-        map.insert(p, (0, 0, 0));
-        let (row, col) = p;
+    for (dir, steps) in instructions {
+        let vector = match dir {
+            'U' => vector!(0, -1),
+            'D' => vector!(0, 1),
+            'L' => vector!(-1, 0),
+            'R' => vector!(1, 0),
+            _ => unreachable!(),
+        };
 
-        let adjacent = vec![
-            (row + 1, col),
-            (row - 1, col),
-            (row, col - 1),
-            (row, col + 1),
-        ];
-
-        for adj in adjacent {
-            if let Some(_rgb) = map.get(&adj) {
-                // already filled
-            } else {
-                unfilled.push(adj);
-            }
-        }
+        let scaled_vector = vector * steps;
+        pos += scaled_vector;
+        perimeter += steps;
+        area += pos.x() * scaled_vector.y();
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_name() {
-        let ex = "\
-R 6 (#70c710)
-D 5 (#0dc571)
-L 2 (#5713f0)
-D 2 (#d2c081)
-R 2 (#59c680)
-D 2 (#411b91)
-L 5 (#8ceee2)
-U 2 (#caa173)
-L 1 (#1b58a2)
-U 2 (#caa171)
-R 2 (#7807d2)
-U 3 (#a77fa3)
-L 2 (#015232)
-U 2 (#7a21e3)
-";
-        assert_eq!(0, solve_p1(ex));
-    }
+    area + perimeter / 2 + 1
 }
